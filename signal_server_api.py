5ad0f21af44cd773406fc02f9ddd75c5a1e3706a
mrbeaxt <ahadhussainsharif@gmail.com>
2026-05-08 21:46:55 +0600

    Update signal_server_api.py

--- signal_server_api.py
+++ signal_server_api.py
@@ -1,8 +1,3 @@
-# Decompiled with PyLingual (https://pylingual.io)
-# Internal filename: manager\signal_provider.py
-# Bytecode version: 3.10.0rc2 (3439)
-# Source timestamp: 1970-01-01 00:00:00 UTC (0)
-
 import json
 import logging
 from time import time
